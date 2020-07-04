f221bc9948e2d4f2c2415b9a3cad4b27dd4027b7
marezgui <m.rezgui@outlook.fr>
2020-07-04 18:40:09 +0200

    Grid component added

---
+++ src/client/components/Grid/Grid.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import Square from '../Square/Square';
+import { makeStyles } from '@material-ui/core/styles';
+
+const ROWS = 20;
+const COLS = 10;
+
+const useStyles = makeStyles((theme) => ({
+    grid: {
+        backgroundImage: 'linear-gradient(63deg, #55acb2, #68c4b6)', 
+        padding: '4px',
+        borderRadius: '12px',
+        width: 'fit-content',
+        margin: 'auto'
+    }
+}));
+
+const Grid = () => {
+    const [gridArray, setGridArray] = useState(new Array(ROWS).fill(null).map(item =>(new Array(COLS).fill(null))));
+    const classes = useStyles();
+
+    console.log(gridArray);
+
+    return (
+        <div
+            className={classes.grid}
+        >
+            {gridArray.map((value, i) => {
+                return (
+                    <div key={i} style={{ display: 'flex' }}>
+                        {gridArray[i].map((value, j) => {
+                            return <Square key={`${i}${j}`} />
+                        })}
+                    </div>
+                );
+                
+            })}
+        </div>
+    );
+}
+
+export default Grid;
--- src/client/pages/Home/Home.tsx
+++ src/client/pages/Home/Home.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Layout from '../../components/Layout/Layout';
+import Grid from '../../components/Grid/Grid';
 
 const Home = () => {
     return (
         <Layout>
-            HOME
+            <Grid />
         </Layout>
     );
 };
