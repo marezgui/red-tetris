22e18af088bf3c1c7d15799b73e86937072f6b88
marezgui <m.rezgui@outlook.fr>
2020-07-08 16:27:49 +0200

    update grid story

--- src/client/components/Grid/stories.tsx
+++ src/client/components/Grid/stories.tsx
@@ -1,12 +1,16 @@
-import * as React from "react";
+import React, { useState } from "react";
 import Grid from ".";
 import { storiesOf } from "@storybook/react";
+
+const ROWS = 20; // X
+const COLS = 10; // Y
 
 const stories = storiesOf("Grid", module) as any;
 
 stories.addWithJSX("Default", () => {
+  const [gridArray, setGridArray] = useState(new Array(ROWS).fill(null).map(item =>(new Array(COLS).fill(null))));
 
   return (
-    <Grid />
+    <Grid gridArray={gridArray} />
   );
 });
