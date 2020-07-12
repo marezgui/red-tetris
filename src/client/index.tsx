85c625f46cc3ce435808d1a59ac926d0dcb7936a
marezgui <m.rezgui@outlook.fr>
2020-07-12 02:15:03 +0200

    add socket hook

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import io from "socket.io-client";
+import SocketContext from './global/SocketContext';
+
+const socket = io(process.env.REACT_APP_SOCKET_SERVER || "http://localhost:5000");
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SocketContext.Provider value={socket}>
+      <App />
+    </SocketContext.Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
